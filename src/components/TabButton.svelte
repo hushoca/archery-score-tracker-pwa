565beb9faf126551db7319a8e5a0f3cab3debed1
hhoca <hushoca@yahoo.com>
2023-02-11 20:29:52 +0000

    fixing an issue where tabbutton background and text color were the same

--- src/components/TabButton.svelte
+++ src/components/TabButton.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<svelte:element this={href ? "a" : "button"} {href} class="p-2  rounded-md text-sm flex items-center justify-center flex-grow border-2 border-blue-700 text-blue-700" on:click
+<svelte:element this={href ? "a" : "button"} {href} class="p-2  rounded-md text-sm flex items-center justify-center flex-grow border-2 border-blue-700" on:click
     class:bg-blue-700={active} class:text-blue-50={active}>
     {value}
 </svelte:element>
